b8f37e47b4400f88c58eb4e15159408b8413933a
RuchiPatel12 <157278334+RuchiPatel12@users.noreply.github.com>
2024-04-04 21:50:59 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,46 @@
+console.log('hello');
+var a='hello world';
+let b=1000;
+document.write("hello");
+// document.write(a);
+// document.write(b);
+function fun(){
+    console.log("learn Js");
+    var c='Javascript';
+    document.write(c);
+}
+fun();
+
+let x='hello';
+let y=12;
+let z= x===y;
+console.log(z);
+// document.write(z);
+function add(){
+    var a=10;
+    var b=20;
+    console.log(a+b);
+}
+add();
+// console.log(add());
+// function assignment
+const f = function add(){
+    var a=10;
+    var b=20;
+    console.log(a+b);
+}
+//  f();
+//  console.log(f());
+//creating object
+const obj=new Object();
+obj[1]='one';
+obj[2]='two';
+obj[3]='three';
+obj[4]='four';
+console.log(obj);
+var value=prompt('enter your name');
+console.log(value);
+alert('hello');
+
+
+
